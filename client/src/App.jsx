c0f5d93559cc65a14fedb816f1edc1e1469bfbf1
Marty <martyrabord@yahoo.fr>
2023-01-11 17:44:00 +0100

    login and home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,28 @@
 import { useState } from 'react';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import './App.css'
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Login from './views/Login';
+import Home from './views/Home';
+
 
 
 function App() {
-    const [count, setCount] = useState(0)
+
 
 
     return (
 
         <div className="App">
 
-            
+            {/* <Login /> */}
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/" element={<Home />} />
+                {/* <Route path="/search" element={<SearchCollaborator />} /> */}
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
         </div>
 
     )
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from "react-redux";
+import store from "./features/store";
+import { HashRouter } from "react-router-dom";
+
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
   </React.StrictMode>,
 )
